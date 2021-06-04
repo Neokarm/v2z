1f4d277aa17ff52cf56b8db26e8790016d6a7dd1
Ilia Feldgun <ilia@neokarm.com>
2021-06-04 18:47:15 +0300

    Redesigned to work with su

--- setup.sh
+++ setup.sh
@@ -9,20 +9,20 @@ function installRPMs(){
 }
 function installPip(){
     curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
-    python get-pip.py
+    sudo python get-pip.py
     rm get-pip.py
 }
 function installPipPackages(){
-    pip install setuptools --upgrade
-    pip install python-neutronclient==3.1.0
+    sudo pip install setuptools --upgrade
+    sudo pip install python-neutronclient==3.1.0
 }
 function installPowerShell(){
     curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
     sudo yum install -y powershell
-    pwsh -command 'Set-PSRepository PSGallery -InstallationPolicy Trusted'
-    pwsh -command 'Install-Module -Name VMware.PowerCLI -Scope CurrentUser'
-    pwsh -command 'Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $true -Confirm:$false'
-    pwsh -command 'Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Scope User -Confirm:$false'
+    sudo pwsh -command 'Set-PSRepository PSGallery -InstallationPolicy Trusted'
+    sudo pwsh -command 'Install-Module -Name VMware.PowerCLI -Scope AllUsers'
+    sudo pwsh -command 'Set-PowerCLIConfiguration -Scope AllUsers -ParticipateInCEIP $true -Confirm:$false'
+    sudo pwsh -command 'Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Scope AllUsers -Confirm:$false'
 }
 installRPMs
 installPip
