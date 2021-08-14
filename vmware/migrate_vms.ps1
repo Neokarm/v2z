2642f2e664b5ed248dfefa482ff2e76503166452
Ilia Feldgun <ilia@neokarm.com>
2021-08-14 17:16:12 +0300

    Logging conversion

--- vmware/migrate_vms.ps1
+++ vmware/migrate_vms.ps1
@@ -12,17 +12,18 @@ function Exit-Script () {
 
 function Convert-Disk($source_path, $source_file, $target_path, $target_file, $temp_directory) {
     Remove-Item -Path "$temp_directory/$source_file-sda"
-    Invoke-Expression "ntfsfix -d $source_path/$sourcefile"
-    Invoke-Expression "export LIBGUESTFS_BACKEND_SETTINGS=force_tcg ; export LIBGUESTFS_CACHEDIR=$temp_directory ;  virt-v2v -i disk $source_path/$source_file -o local -os $temp_directory"
-    Invoke-Expression "dd if=$temp_directory/$source_file-sda bs=128M | pv | dd of=$target_path/$target_file bs=128M"
+    $ntfsfix_command = "ntfsfix -d $source_path/$sourcefile"
+    Write-Log $ntfsfix_command
+    Invoke-Expression $ntfsfix_command | Write-Log
+    $virt_v2v_command = "export LIBGUESTFS_BACKEND_SETTINGS=force_tcg ; export LIBGUESTFS_CACHEDIR=$temp_directory ;  virt-v2v -i disk $source_path/$source_file -o local -os $temp_directory"
+    Write-Log $virt_v2v_command
+    Invoke-Expression $virt_v2v_command | Write-Log
+    $dd_command = "dd if=$temp_directory/$source_file-sda bs=128M | pv | dd of=$target_path/$target_file bs=128M"
+    Invoke-Expression $dd_command | Write-Log
     Invoke-Expression "sync"
 }
 function Convert-DiskFromNFS($vmdk_filename, $nfs_path, $target_path, $target_file, $temp_directory) {
-    Remove-Item -Path "$temp_directory/$vmdk_filename-sda"
-    Invoke-Expression "ntfsfix -d $nfs_path/$vmdk_filename"
-    Invoke-Expression "export LIBGUESTFS_BACKEND_SETTINGS=force_tcg ; export LIBGUESTFS_CACHEDIR=$temp_directory ;  virt-v2v -i disk $nfs_path/$source_file -o local -os $temp_directory"
-    Invoke-Expression "dd if=$temp_directory/$vmdk_filename-sda bs=128M | pv | dd of=$target_path/$target_file bs=128M"
-    Invoke-Expression "sync"
+    # TODO
 }
 
 $SYMP_COMMAND="/usr/bin/symp -q -k --url https://$SYMPIP -u $SYMPUSER -p $SYMPPASS -d $SYMPTENANT $SYMPPROJECT"
