39a170f4f36897d9ccbbbc25e03d6bdd64ed4c24
Ilia Feldgun <ilia@neokarm.com>
2021-08-15 14:09:29 +0300

    Better curl

--- vmware/vmware.ps1
+++ vmware/vmware.ps1
@@ -54,9 +54,11 @@ function Copy-DiskFromVmware($disk, $target_device) {
     $ESX_CRED = New-Object System.Management.Automation.PSCredential -ArgumentList ($ESXUSER, $password)
     # Invoke-WebRequest -SkipCertificateCheck -TransferEncoding gzip -Uri $uri -Credential $ESX_CRED -OutFile "/dev/${target_device}" | Write-Log
     # $curl_command = "curl -u ${ESXUSER}:${ESXPASSWORD} ${uri} --insecure --compressed > ./vmdkkks/dev/${target_device}"
-    $curl_command = "curl -v -u ${ESXUSER}:${ESXPASSWORD} ${uri} --insecure --compressed > ./vmdkkks"
-    Write-Log "Curl command: $curl_command"
-    Invoke-Expression $curl_command | Write-Log
+    $curl_command = "curl"
+    $arguments = "-v -u ${ESXUSER}:${ESXPASSWORD} ${uri} --insecure --compressed -o /dev/${target_device}"
+    Write-Log "Curl command: $curl_command + $arguments"
+    Start-Process -FilePath 'curl' -ArgumentList $arguments -Wait | Write-Log
+    # Invoke-Expression $curl_command | Write-Log
     #curl -u root:Str@to2014 https://10.16.1.105/folder/Cloud_Mgmt_Tool_Fedora23/Cloud_Mg?dcPath=ha-datacenter\&dsName=VPSA_it_prod_datastore1_yaffo -SkipCertificateCheck --insecure --compressed > /dev/vdg
     #curl -u root:Str@to2014 https://10.16.1.105/folder/Win10x64pro/Win10x64pro-flat.vmdk?dcPath=ha-datacenter\&dsName=VPSA_it_prod_datastore1_yaffo --insecure --compressed > /dev/vdd
     #PS /home/centos/v2v-tools/vmware> $disk  
