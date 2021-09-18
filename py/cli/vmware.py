98dd1a8078080c50be2e2d7d1bdeac271ae469e1
Ilia Feldgun <ilia@neokarm.com>
2021-09-18 21:34:41 +0300

    MOre docstrings

--- py/cli/vmware.py
+++ py/cli/vmware.py
@@ -8,7 +8,7 @@ app = typer.Typer()
 
 @app.command()
 def get_vm(name: str = "", folder_name: str = ""):
-    """Get vms
+    """Get vms from vmware
 
     Args:
         name (str, optional): Get vm by name. Defaults to "".
@@ -39,7 +39,21 @@ def get_vm(name: str = "", folder_name: str = ""):
 
 
 @app.command()
-def get_vm_disks(vm_name: str):
+def get_vm_disks(vm_name: str) -> list:
+    """Get the disks of the vm from vmware
+
+    Args:
+        vm_name (str): Name of the vm in vmware
+
+    Returns:
+        list[{
+                name: str,
+                vm: str,
+                capacity_gb: float,
+                datastore: str,
+                vmdk_path: str
+            }]: Disk(s) from vmware
+    """
     vsphere_powershell = vmware.PowerShellVsphere(config.VIHOST,
                                                   config.VIUSER,
                                                   config.VIPASSWORD)
@@ -50,7 +64,19 @@ def get_vm_disks(vm_name: str):
 
 
 @app.command()
-def curl_vmdk(datastore: str, vmdk_path: str, output_path: str):
+def curl_vmdk(datastore: str, vmdk_path: str, output_path: str) -> str:
+    """Uses curl command to receive vmdk from vmware esxi.
+       Make sure the vm is offline
+
+    Args:
+        datastore (str): The name of the datastore of the vmdk
+        vmdk_path (str): The path of the vmdk in the datastore.
+                         Usually vm_name/vm_name(_1)-flat.vmdk
+        output_path (str): The target file path
+
+    Returns:
+        str: output_path
+    """
     output_path = vmware.curl_vmdk_file(datastore,
                                         vmdk_path,
                                         output_path,
