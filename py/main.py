c266eb0575010a410f91e57a4cf98d6b5c476838
Ilia Feldgun <ilia@neokarm.com>
2021-09-23 17:02:07 +0300

    Logging through file

--- py/main.py
+++ py/main.py
@@ -1,6 +1,5 @@
 import logging
 import logging.config
-import config
 import typer
 
 import cli.autopilot
@@ -8,41 +7,8 @@ import cli.v2v
 import cli.vmware
 import cli.zcompute
 
-# TODO: Move this to a file (logging.conf)
-logging_config = {
-    "version": 1,
-    "root": {
-        "handlers": ["file"],
-        "level": "DEBUG"
-    },
-    "handlers": {
-        "console": {
-            "formatter": "just_message",
-            "class": "logging.StreamHandler",
-            "level": "WARNING"
-        },
-        "file": {
-            "formatter": "simple",
-            "class": "logging.FileHandler",
-            "level": "DEBUG",
-            "filename": config.LOG
-        }
-    },
-    "formatters": {
-        "simple": {
-            "format": "%(asctime)s : %(levelname)s : Log : %(message)s :"
-                      " %(module)s : %(funcName)s : %(lineno)d",
-            "datefmt": "%d-%m-%Y %I:%M:%S"
-        },
-        "just_message": {
-            "format": "%(asctime)s : %(levelname)s : %(message)s",
-            "datefmt": "%d-%m-%Y %I:%M:%S"
-        }
-    }
-}
-logging.getLogger(__name__)
-logging.config.dictConfig(logging_config)
-
+logging.config.fileConfig("logging.conf",
+                          disable_existing_loggers=False)
 
 app = typer.Typer()
 app.add_typer(cli.vmware.app, name="vmware")
