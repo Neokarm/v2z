f2feee06a8c6d8cffaba3424eb7f80428a050d69
Ilia Feldgun <ilia@neokarm.com>
2021-10-21 17:09:32 +0300

    Module paths fixed

--- py/cli/v2v.py
+++ py/cli/v2v.py
@@ -18,7 +18,7 @@ def convert_vmdk(vmdk_path: str, output_path: str) -> str:
         str: Path to the output raw file
     """
 
-    output_file_path = v2v.virt_v2v.vmdk_to_raw(vmdk_path, output_path)
+    output_file_path = v2v.vmdk_to_raw(vmdk_path, output_path)
     logging.debug(f"raw file path: {output_file_path}")
     typer.echo(output_file_path)
     return (output_file_path)
@@ -37,8 +37,7 @@ def convert_vhd(vhd_path: str, output_path: str) -> str:
         str: Path to the output raw file
     """
 
-    output_file_path = v2v.virt_v2v.vhd_to_raw(vhd_path, output_path,
-                                               is_nfs=is_nfs)
+    output_file_path = v2v.vhd_to_raw(vhd_path, output_path)
     logging.debug(f"raw file path: {output_file_path}")
     typer.echo(output_file_path)
     return (output_file_path)
@@ -52,7 +51,7 @@ def dd_disk(raw_file: str, block_device: str):
         raw_file (str): Path to the input file
         block_device (str): Path to the output file
     """
-    v2v.virt_v2v.dd_disk(raw_file, block_device)
+    v2v.dd_disk(raw_file, block_device)
 
 
 if __name__ == "__main__":
